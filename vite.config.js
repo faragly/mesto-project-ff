3f5daca74ae8437b59defddc85ad1394edc01142
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2024-10-15 11:05:24 +0300

    moved index.html -> src/index.html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/mesto-project-ff/',
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'src/index.html'),
+    },
+  },
 });
